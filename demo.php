70427d07f459879eca63311cd195b211f6597829
WinHtetKyaw15 <winwinhtetkyaw.qualymm@gmail.com>
2022-09-30 09:55:59 +0630

    adding and mixing html

--- demo.php
+++ demo.php
@@ -6,6 +6,14 @@ get_header();
 if(have_posts()){
   while (have_posts()) {
     the_post();
+    ?>
+    <article id="<?php the_ID()?>" <?php post_class()?>>
+      <?php the_content(); ?>
+    </article>
+    <!-- // echo '<article id="'. the_ID() . '"' . post_class() . '>'; -->
+      <!-- // the_content(); -->
+    <!-- // echo '</article>'; -->
+    <?php
   }
 } else {
   //.What happens if there are no posts
